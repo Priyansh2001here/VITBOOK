74d0fed5cc0a89b5fe3409c51ef354eddaad1844
Priyansh <singhpriyansh51001@gmail.com>
2020-10-09 10:48:20 +0530

    Follow Unflollow ajax button

--- social/urls.py
+++ social/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path('notification/', views.notification, name='notification'),
 
     # path('chat/', views.chat),
-    path('chat/',  views.ChatView.as_view(), name='chat_ajax'),
-    path('chat/create/',  views.CreateChatUser.as_view(), name='chat_ajax_create'),
+    path('chat/', views.ChatView.as_view(), name='chat_ajax'),
+    path('chat/create/', views.CreateChatUser.as_view(), name='chat_ajax_create'),
     # path('chat/upload', views.chatUpload),
-    
+
     path('ub/', views.ub, name='ub'),
     path('ub/upload', views.Ubupload),
 
@@ -37,20 +37,21 @@ urlpatterns = [
 
     #     path('photos/', views.PhotosView.as_view()),
 
-
     path('post/<int:pk>', views.MyPostDetailView.as_view(), name='postDetail'),
     path('post/like/<int:pk>/<page>', views.like, name="like"),
     path('post/unlike/<int:pk>/<page>', views.unlike, name='unlike'),
 
     path('profile/edit/<int:pk>',
          views.MyProfileUpdateView.as_view(success_url="/social/home"), name='profileEdit'),
+    # -----------------------------------------------------------------------#
     path('profile/', views.MyProfileListView.as_view(), name='profileList'),
     path('profile/<int:pk>', views.profiling, name='profileDetail'),
     # path('myprofile/<int:pk>', views.MyProfileDetailView.as_view(),),
 
-    
-    path('profile/follow/<int:pk>', views.follow, name='follow'),
-    path('profile/unfollow/<int:pk>', views.unfollow, name='unfollow'),
+    # --------------------------------------------------------------------#
+
+    path('profile/action', views.action),
+    # --------------------------------------------------------------------#
 
     path('vithub/create/', views.VithubCreate.as_view(success_url="/social/vithub"), name='vithubCreate'),
     path('vithub/delete/<int:pk>',
@@ -58,11 +59,9 @@ urlpatterns = [
     path('vithub/', views.VithubListView.as_view(), name='vithubList'),
     path('vithub/<int:pk>', views.VithubDetailView.as_view(), name='vithubDetail'),
 
-
     path('poll/', views.poll_home, name='poll_home'),
     path('poll/create/', views.poll_create, name='poll_create'),
     path('poll/vote/<poll_id>/', views.poll_vote, name='poll_vote'),
     path('poll/results/<poll_id>/', views.poll_results, name='poll_results'),
-
 
 ]
